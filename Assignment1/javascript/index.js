801face0bfd0165f3224d15550862bb7b6894104
Dice1111 <kingdice1111@gmail.com>
2023-04-17 19:53:32 +0800

    find&replace

---
+++ Assignment1/javascript/index.js
@@ -0,0 +1,31 @@
+
+
+function getCurrentDate(){
+    const date = new Date();
+    let currentDay= String(date.getDate()).padStart(2, "0");
+    let currentMonth = String(date.getMonth()+1).padStart(2,"0");
+    let currentYear = date.getFullYear();
+    let currentDate = currentDay+"-"+currentMonth+"-"+currentYear;
+    document.getElementById("date").value = currentDate;
+}
+
+function inputEnable(){
+    let find = document.getElementById("find");
+    document.getElementById("replace").disabled = false;
+    if(find.value == ""){
+        document.getElementById("replace").disabled = true;
+    }
+}
+
+function findAndReplace(){
+    let find = document.getElementById("find").value.trim();
+    let replace = document.getElementById("replace").value.trim();
+    let message = document.getElementById("message").value.trim();
+
+    if(message.includes(find)){
+        document.getElementById("message").value = message.replaceAll(find,replace);
+        
+    }else{
+        alert("Not Found!");
+    }
+}
