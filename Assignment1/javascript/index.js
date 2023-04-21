81c1eb9de534c6df69bb3bb53a9328465ce2cbc9
Dice1111 <kingdice1111@gmail.com>
2023-04-21 14:18:49 +0800

    validate

--- Assignment1/javascript/index.js
+++ Assignment1/javascript/index.js
@@ -44,16 +44,40 @@ function findAndReplace(){
 }
 
 function validateForm(){
-    var input = document.getElementById("message").value;
+    let inputName = document.getElementById("name").value;
+    let inputCode = document.getElementById("moduleCode").value;
+    let inputMes = document.getElementById("message").value;
 
-    if(input=="" || input == null){
-        alert("pls enter name.");
-        return false;
+    let valid = true;
+
+    if(inputName=="" || inputName == null){
+        document.getElementById("errorName").innerHTML = "Text box must not be empty."
+        valid = false;
 
     }
-    else if(/[A-Za-z0-9]+/.test(input) == false){
-        alert("input does not match the pattern");
-    return false;
+    else if(/[A-Za-z0-9]+/.test(inputName) == false){
+        document.getElementById("errorName").innerHTML = "Cannot enter special character."
+        valid = false;
+    }else{
+        document.getElementById("errorName").innerHTML = "";
     }
-    return true;
+
+    if(inputCode=="" || inputCode == null){
+        valid = true;
+    }
+
+    else if(/^[A-Z][a-z][2-9]{3}$/.test(inputCode) == false){
+        document.getElementById("errorCode").innerHTML = "the content must start with 1 upper case and 1 lower case alphabets and follows by 3 digits in the range of 2 to 9."
+        valid = false;
+    }else{
+        document.getElementById("errorCode").innerHTML = "";
+    }
+
+    if(inputMes=="" || inputMes == null){
+        document.getElementById("errorMes").innerHTML = "Text box must not be empty.";
+        valid = false;
+    }else{
+        document.getElementById("errorMes").innerHTML = "";
+    }
+    return valid;
     }
